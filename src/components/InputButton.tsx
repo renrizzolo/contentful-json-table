d7f1b4382cf72e5ad0aeb433836e7e4c449c2dba
Ren Rizzolo <renrizmusic@gmail.com>
2019-10-22 14:55:09 +1100

    use IconButton

--- src/components/InputButton.tsx
+++ src/components/InputButton.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@contentful/forma-36-react-components';
+import { IconButton } from '@contentful/forma-36-react-components';
 
 export const InputButton = ({ onClick, icon, style }) => (
-  <Button
-    buttonType="naked"
-    size="small"
-    icon={icon}
-    style={{ width: 32, position: 'absolute', zIndex: 2, right: -14, top: 12, cursor: 'pointer', ...style}}
-    onClick={onClick}
-  />
-);
+         <IconButton
+         iconProps={{className: '', icon, }}
+           buttonType="naked"
+           size="small"
+           icon={icon}
+           style={{
+            //  width: 32,
+             position: 'absolute',
+             zIndex: 2,
+             right: -10,
+             top: 16,
+             cursor: 'pointer',
+             ...style
+           }}
+           onClick={onClick}
+         />
+       );
 
 InputButton.propTypes = {
   onClick: PropTypes.func,
